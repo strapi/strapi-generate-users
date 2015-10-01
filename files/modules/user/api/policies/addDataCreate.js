a5dc15cbde6e4b28263981cc56faae0268234152
Pierre Burgy <pierreburgy@MacBook-Air-de-Pierre.local>
2015-10-01 15:46:36 +0200

    Fix bugs contributors addDataCreate policy

--- files/modules/user/api/policies/addDataCreate.js
+++ files/modules/user/api/policies/addDataCreate.js
@@ -3,7 +3,7 @@
 exports.addDataCreate = function * (next) {
   this.request.body.createdBy = this.user && this.user.id;
   this.request.body.updatedBy = this.user && this.user.id;
-  this.request.body.contributors = this.request.body.contributors || [this.user];
+  this.request.body.contributors = this.request.body.contributors && this.request.body.contributors.length ?this.request.body.contributors : [this.user];
 
   yield next;
 };
